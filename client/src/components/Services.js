71a908e34cfc47a7ad142724ab521333f65500e4
Sevval Senel <ssevvalsenel@gmail.com>
2023-06-26 18:51:54 +0300

    draft code

---
+++ client/src/components/Services.js
@@ -0,0 +1,22 @@
+import React from "react";
+//icon
+import { BsArrowUpRight } from "react-icons/bs";
+//motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
+
+//services data
+const services = [{}];
+
+const Services = () => {
+  return (
+    <div className="section" id="services">
+      Services Lorem, ipsun dolor sit amet consectetur adipisicing elit.
+      Malores, quia quo expadita accousamus illum ducimus tempore repaliat
+      distinctio harum aperiam.
+    </div>
+  );
+};
+
+export default Services;
