ead35110c20bc93a8dc06d5f7220b69e551583c4
Sevval Senel <ssevvalsenel@gmail.com>
2023-06-27 00:55:44 +0300

    removed logo, added icons

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
-//images
-import Logo from "../assets/logo.svg";
+//icons
+import { PiGithubLogo, PiLinkedinLogo } from "react-icons/pi";
 
 const Header = () => {
   return (
@@ -8,11 +8,32 @@ const Header = () => {
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           {/* logo */}
-          <a href="#">
+          {/* <a href="#">
             <img src={Logo} alt="" />
-          </a>
+          </a> */}
+          <div>
+            {/* <h3  className="mb-6 text-[36px] lg:text-[30px] font-primary uppercase leading-[1] ">
+              Şevval Şenel
+            </h3> */}
+          </div>
           {/* button */}
-          <button className="btn btn-sm" >Work with me</button>
+          {/* <button className="btn btn-sm" >Work with me</button> */}
+          <div className="flex space-x-2">
+            <a
+              href="https://github.com/ssevvalsenel"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <PiGithubLogo className="text-3xl" />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/sevvalsenel/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <PiLinkedinLogo className="text-3xl" />
+            </a>
+          </div>
         </div>
       </div>
     </header>
